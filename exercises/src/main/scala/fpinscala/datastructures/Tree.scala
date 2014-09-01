e728e2a9606e0e67f2d3e45366e5cc28cb06e9de
takc923 <takc923@gmail.com>
2014-09-02 02:24:25 +0900

    implement Tree and Option.

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -1,13 +1,53 @@
 package fpinscala.datastructures
 
 sealed trait Tree[+A]
+
 case class Leaf[A](value: A) extends Tree[A]
+
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 
 object Tree {
 
+  def size[A](t: Tree[A]): Int =
+    t match {
+      case Leaf(_) => 1
+      case Branch(l, r) => size(l) + size(r)
+    }
 
+  def maximum(t: Tree[Int]): Int =
+    t match {
+      case Leaf(a) => a
+      case Branch(l, r) => maximum(l) max maximum(r)
+    }
 
+  def depth[A](t: Tree[A]): Int =
+    t match {
+      case Leaf(_) => 1
+      case Branch(l, r) => 1 + (depth(l) max depth(r))
+    }
 
+  def map[A, B](t: Tree[A], f: A => B): Tree[B] =
+    t match {
+      case Leaf(a) => Leaf(f(a))
+      case Branch(l, r) => Branch(map(l, f), map(r, f))
+    }
+
+  def fold[A, B](t: Tree[A], f: A => B)(g: (B, B) => B): B =
+    t match {
+      case Leaf(a) => f(a)
+      case Branch(l, r) => g(fold(l, f)(g), fold(r, f)(g))
+    }
+
+  def sizeByFold[A](t: Tree[A]): Int =
+    fold[A, Int](t, _ => 1)(_ + _)
+
+  def maximumByFold(t: Tree[Int]): Int =
+    fold[Int, Int](t, n => n)(_ max _)
+
+  def depthByFold[A](t: Tree[A]): Int =
+    fold[A, Int](t, _ => 1)((l, r) => 1 + (l max r))
+
+  def mapByFold[A, B](t: Tree[A], f: A => B): Tree[B] =
+    fold[A, Tree[B]](t, v => Leaf(f(v)))(Branch(_, _))
 }
--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -1,18 +1,33 @@
 package fpinscala.errorhandling
 
 
-import scala.{Option => _, Some => _, Either => _, _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
+import scala.{Option => _, Some => _, None => _, Either => _, _} // hide std library `Option` and `Either`, since we are writing our own in this chapter
 
 sealed trait Option[+A] {
-  def map[B](f: A => B): Option[B] = sys.error("todo")
+  def map[B](f: A => B): Option[B] = this match {
+    case Some(v) => Some(f(v))
+    case None => None
+  }
 
-  def getOrElse[B>:A](default: => B): B = sys.error("todo")
+  def getOrElse[B>:A](default: => B): B = this match {
+    case Some(v) => v
+    case None => default
+  }
 
-  def flatMap[B](f: A => Option[B]): Option[B] = sys.error("todo")
+  def flatMap[B](f: A => Option[B]): Option[B] = this match {
+    case Some(v) => f(v)
+    case None => None
+  }
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = sys.error("todo")
+  def orElse[B>:A](ob: => Option[B]): Option[B] = this match {
+    case s :Some => s
+    case None => ob
+  }
 
-  def filter(f: A => Boolean): Option[A] = sys.error("todo")
+  def filter(f: A => Boolean): Option[A] = this match {
+    case Some(v) if f(v) => Some(v)
+    case _ => None
+  }
 }
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
@@ -38,7 +53,10 @@ object Option {
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
-  def variance(xs: Seq[Double]): Option[Double] = sys.error("todo")
+
+  def variance(xs: Seq[Double]): Option[Double] = mean(xs) flatMap {
+    m => mean(xs.map(v => math.pow(m - v, 2)))
+  }
 
   def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = sys.error("todo")
 
